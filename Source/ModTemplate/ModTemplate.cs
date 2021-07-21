3d44ab1a14f0fd6b7e44307220f35bb7302a02b4
Travis Martin <tjmartin2003@gmail.com>
2021-07-20 23:22:29 -0500

    Example settings.

---
+++ Source/ModTemplate/ModTemplate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ModTemplate {
+	public class ModTemplate : Mod {
+		public static ModTemplate Instance;
+
+		public Settings settings;
+
+		public ModTemplate(ModContentPack content) : base(content) {
+			if (ModTemplate.Instance == null) {
+				ModTemplate.Instance = this;
+			}
+
+			this.settings = this.GetSettings<Settings>();
+		}
+
+		public override void DoSettingsWindowContents(Rect rect) {
+			Listing_Standard listingStandard = new Listing_Standard();
+			listingStandard.Begin(rect);
+			_ = listingStandard.Label("Example setting");
+			this.settings.exampleSetting = listingStandard.Slider(this.settings.exampleSetting, 0, 100);
+			listingStandard.End();
+
+			base.DoSettingsWindowContents(rect);
+		}
+	}
+}
---
+++ Source/ModTemplate/Settings.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace ModTemplate {
+	public class Settings : ModSettings {
+		public float exampleSetting = 1;
+
+		public override void ExposeData() {
+			Scribe_Values.Look(ref this.exampleSetting, "exampleSetting");
+
+			base.ExposeData();
+		}
+	}
+}
